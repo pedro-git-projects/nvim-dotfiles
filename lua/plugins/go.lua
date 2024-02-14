1f7bf577f96aeb20148a43e1e73aa95b13deb926
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-02-13 22:20:48 -0300

    format and import on save golang

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -4,6 +4,14 @@ return {
     "ray-x/guihua.lua",
   },
   config = function()
+    local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require('go.format').goimport()
+      end,
+      group = format_sync_grp,
+    })
     require("go").setup()
   end,
   event = { "CmdlineEnter" },
